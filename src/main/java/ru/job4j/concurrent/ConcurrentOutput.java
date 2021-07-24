e9bb368dddfcc7bbdbf873f805207606a039cb9a
nikolay <Mosquitod3@yandex.ru>
2021-07-24 16:04:09 +0300

    Запуск нити. Thread.start().

---
+++ src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -0,0 +1,16 @@
+package ru.job4j.concurrent;
+
+public class ConcurrentOutput {
+    public static void main(String[] args) {
+        Thread another = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+                );
+        another.start();
+
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName());
+        }).start();
+
+        System.out.println(Thread.currentThread().getName());
+    }
+}
