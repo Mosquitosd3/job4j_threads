833f74a3a8fef139897d71bd9ee87fb8321fbf2f
nikolay <Mosquitod3@yandex.ru>
2021-07-24 17:36:01 +0300

    Прерывание нити

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,27 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable{
+    @Override
+    public void run() {
+        int count = 0;
+        String[] process = {"--", "\\", "|", "/"};
+        while (!Thread.currentThread().isInterrupted()) {
+            if (count == 4) {
+                count = 0;
+            }
+            System.out.print("\n load: " + process[count++]);
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+       Thread progress = new Thread(new ConsoleProgress());
+       progress.start();
+       Thread.sleep(5000);
+       progress.interrupt();
+    }
+}
