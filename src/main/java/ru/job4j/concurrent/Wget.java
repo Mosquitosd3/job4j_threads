88b0db4a741774389c5de6d76d2e623d0390df8f
nikolay <Mosquitod3@yandex.ru>
2021-07-25 16:20:23 +0300

    Скачивание файла с ограничением. refactoring

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -3,6 +3,7 @@ package ru.job4j.concurrent;
 import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class Wget implements Runnable {
@@ -17,24 +18,35 @@ public class Wget implements Runnable {
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+             FileOutputStream fileOutputStream = new FileOutputStream(getNameFile())) {
             byte[] dataBuffer = new byte[1024];
             int byteRead;
+            long startTime = System.currentTimeMillis();
             while ((byteRead = in.read(dataBuffer, 0, 1024)) != -1) {
-                long startTime = System.currentTimeMillis();
                 fileOutputStream.write(dataBuffer, 0, byteRead);
                 long endTime = System.currentTimeMillis();
-                long time = startTime - endTime;
+                long time = endTime - startTime;
                 if (speed < time) {
                     Thread.sleep(time - speed);
                 }
+                startTime = System.currentTimeMillis();
             }
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
 
+    private String getNameFile() throws MalformedURLException {
+        URL address = new URL(url);
+        String name = address.getFile();
+        return name.substring(name.lastIndexOf("/") + 1);
+
+    }
+
     public static void main(String[] args) throws InterruptedException {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Enter url and speed");
+        }
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
         Thread wget = new Thread(new Wget(url, speed));
